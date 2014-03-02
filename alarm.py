60b8c67123d98c0b872e86db1cea17c19f471a9c
Tom Camp <tom@campsitemedia.com>
2014-03-02 14:58:19 -0800

    iteration tweaks

--- alarm.py
+++ alarm.py
@@ -30,6 +30,11 @@ logging.basicConfig(filename='alarm.log', level=logging.INFO, format='%(asctime)
 
 
 def main():
+    # initialize spi and leds objects
+    #spidev	= file("/dev/spidev0.0", "wb")  # ref to spi connection to the led bar
+    #leds = ledstrip.LEDStrip(pixels = args.leds, spi = spidev)
+    #all_off(leds)
+    leds = 'all'
     while True:
         day = time.strftime('%A')
         start_time = Config.get('StartTimes', day)
@@ -42,13 +47,27 @@ def main():
             now = time.time()
             start_now = time.mktime(datetime.datetime.strptime(start, '%Y-%m-%d %H:%M').timetuple())
 
-            if now > start_now and now < start_now + 10:
-               logging.info('Starting alarm')
-               time.sleep(10)
-               print 'done'
-            
+            if now >= start_now and now <= start_now + 1:
+                print now, start_now
+                logging.info('Starting alarm')
+                alarm_on(leds)
+                time.sleep(120)
+                logging.info('Stopping alarm')
+                all_off(leds)
 
+def alarm_on(leds):
+    colors = {'red':[255, 0, 0], 'orange':[255, 69, 0], 'yellow':[240, 230, 140], 'blue':[135, 206, 235]}
+    phases = ['red', 'orange', 'yellow', 'blue']
+    for phase in phases:
+        for pix in range(32):
+            leds.setPixelColorRGB(pixel = pix, red = colors[phase][0], green = colors[phase][1], blue = colors[phase][1])
+            leds.show()
+            time.sleep(1)
 
+def all_off(leds):
+    for each in range(32):
+        leds.setPixelColorRGB(pixel = each, red = 0, green = 0, blue = 0)
+        leds.show()
 
 if __name__ == "__main__":
     main()
